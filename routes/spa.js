2fd91cf87c724408c9aec6a3390fa890cbb99a58
chouchou723 <527835300@qq.com>
2018-03-13 12:05:35 +0800

    spa

--- routes/spa.js
+++ routes/spa.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var path = require('path')
 // const root = {root:path.join(__dirname,'../public/html')}
-const home = {root:path.join(__dirname,'../public/reactSPA')}
+const spa = {root:path.join(__dirname,'../public/reactSPA')}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   // res.sendFile('index.html',root)
-  res.sendFile('index.html',home)
+  res.sendFile('index.html',spa)
   
 });
 module.exports = router;
