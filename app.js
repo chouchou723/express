0465e096d926d41b82f29d80e7f016244943d1e3
ChouChou <527835300@qq.com>
2018-04-25 16:14:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -88,23 +88,24 @@ Referer:'http://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/eccf0f
   Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 'Accept-Encoding':'gzip, deflate',
 'Accept-Language':'zh-CN,zh;q=0.9',
-'Cache-Control':'max-age=0',
-Connection:'keep-alive',
+	  Connection:'keep-alive',
+Referer:'http://shanghaicity.openservice.kankanews.com/',
+
 Host:'shanghaicity.openservice.kankanews.com',
 'Upgrade-Insecure-Requests':1,
 'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
   }
-  let c1= 'HMACCOUNT=70EC6624AE5CF7B8';
+  let c1,c2,c3;
 	request.get('https://hm.baidu.com/hm.gif?cc=0&ck=1&cl=24-bit&ds=375x667&vl=667&et=0&ja=0&ln=zh-cn&lo=0&lt=1524620683&rnd=719889793&si=6f69830ae7173059e935b61372431b35&v=1.2.30&lv=3&ct=!!&tt=%E4%B8%8A%E6%B5%B7%E5%8F%91%E5%B8%83-%E5%B8%82%E6%94%BF%E5%A4%A7%E5%8E%85&sn=34872')
 		.end((err,hr)=>{
-		let c1 = hr.headers['set-cookie'].join(',').match(/(HMACCOUNT=.+?);/)[1];
+		 c1 = hr.headers['set-cookie'].join(',').match(/(HMACCOUNT=.+?);/)[1];
 		//console.log(hr);
 		console.log(c1);
 		request.get('http://shanghaicity.openservice.kankanews.com')
 		.end((err,hrr)=>{
-			let c2 = hrr.headers['set-cookie'].join(',').match(/(Hm_1vt_6f69830ae7173059e935b61372431b35=.+?);/)[1];
+			 c2 = hrr.headers['set-cookie'].join(',').match(/(Hm_1vt_6f69830ae7173059e935b61372431b35=.+?);/)[1];
 		console.log(c2);
-			let c3 = c1+'; '+c2+ '; _gat=1';
+			 c3 = c1+'; '+c2+ '; _gat=1';
 			console.log(c3);
 					 	request.get('http://shanghaicity.openservice.kankanews.com/public/bus')
 	  .set(base1)
