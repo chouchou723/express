d02365e17f7d009ae73049007f8a29818d8d1615
chouchou723 <527835300@qq.com>
2018-04-26 10:18:53 +0800

    bus

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/', index);
 // app.get('/drrr',(req,res)=>{
 //   res.sendFile(__dirname+'/public/client/index.html')
 // })
-
+//天气接口
 app.get('/weather/:city', (req,res,next)=>{
   let city = req.params.city;
   let arr
@@ -61,8 +61,7 @@ app.get('/weather/:city', (req,res,next)=>{
   })
   .catch(err => console.log(err))
 });
-axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
-axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4';
+//公交接口查询实时
 app.get('/bus/:sid/:direction/:stopId', (req,res,next)=>{
   let sid = req.params.sid;
   let direction = req.params.direction;
@@ -72,39 +71,39 @@ app.get('/bus/:sid/:direction/:stopId', (req,res,next)=>{
   stoptype:direction,
   stopid:stopId,
   sid:sid}
+  let ref = `http://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/${sid}`
   let baseH = {
-  Accept:'*/*',
-'Accept-Encoding':'gzip, deflate',
-'Accept-Language':'zh-CN,zh;q=0.9',
-Connection:'keep-alive',
-'Content-Length':57,
-Host:'shanghaicity.openservice.kankanews.com',
-Origin:'http://shanghaicity.openservice.kankanews.com',
-Referer:'http://shanghaicity.openservice.kankanews.com/public/bus/mes/sid/eccf0f60e0b483f03cdedaa907e01436',
-'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
-'X-Requested-With':'XMLHttpRequest'
+        Accept:'*/*',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9',
+        Connection:'keep-alive',
+        'Content-Length':57,
+        Host:'shanghaicity.openservice.kankanews.com',
+        Origin:'http://shanghaicity.openservice.kankanews.com',
+        Referer:ref,
+        'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
+        'X-Requested-With':'XMLHttpRequest'
   }
   let base1 = {
-  Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-'Accept-Encoding':'gzip, deflate',
-'Accept-Language':'zh-CN,zh;q=0.9',
-	  Connection:'keep-alive',
-Referer:'http://shanghaicity.openservice.kankanews.com/',
+          Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9',
+            Connection:'keep-alive',
+        Referer:'http://shanghaicity.openservice.kankanews.com/',
 
-Host:'shanghaicity.openservice.kankanews.com',
-'Upgrade-Insecure-Requests':1,
-'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
+        Host:'shanghaicity.openservice.kankanews.com',
+        'Upgrade-Insecure-Requests':1,
+        'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
   }
    let base2 = {
-  Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-'Accept-Encoding':'gzip, deflate',
-'Accept-Language':'zh-CN,zh;q=0.9',
-	   'Cache-Control':'max-age=0',
-	  Connection:'keep-alive',
-
-Host:'shanghaicity.openservice.kankanews.com',
-'Upgrade-Insecure-Requests':1,
-'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
+          Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9',
+            'Cache-Control':'max-age=0',
+            Connection:'keep-alive',
+        Host:'shanghaicity.openservice.kankanews.com',
+        'Upgrade-Insecure-Requests':1,
+        'User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
   }
   let c1,c2,c3,c4,c5;
 	request.get('https://hm.baidu.com/hm.gif?cc=0&ck=1&cl=24-bit&ds=375x667&vl=667&et=0&ja=0&ln=zh-cn&lo=0&lt=1524620683&rnd=719889793&si=6f69830ae7173059e935b61372431b35&v=1.2.30&lv=3&ct=!!&tt=%E4%B8%8A%E6%B5%B7%E5%8F%91%E5%B8%83-%E5%B8%82%E6%94%BF%E5%A4%A7%E5%8E%85&sn=34872')
@@ -112,22 +111,22 @@ Host:'shanghaicity.openservice.kankanews.com',
 		 c1 = hr.headers['set-cookie'].join(',').match(/(HMACCOUNT=.+?);/)[1];
 		//console.log(hr);
 		//console.log(c1);
-		request.get('http://shanghaicity.openservice.kankanews.com')
-		.set(base2)
-		.end((err,hrr)=>{
-			//console.log(hrr.headers['set-cookie'])
-			 c2 = hrr.headers['set-cookie'].join(',').match(/(Hm_1vt_6f69830ae7173059e935b61372431b35=.+?);/)[1];
-			 c4 = 'Hm_lvt_6f69830ae7173059e935b61372431b35=1524617432,1524617445,1524619410,1524620683';
-			 c5 = 'Hm_lpvt_6f69830ae7173059e935b61372431b35='+new Date().valueOf();
-		//console.log(c2);
-			 c3 = c1+'; '+c2+ '; _gat=1';
-			//console.log(c3);
+        request.get('http://shanghaicity.openservice.kankanews.com')
+              .set(base2)
+              .end((err,hrr)=>{
+                //console.log(hrr.headers['set-cookie'])
+                c2 = hrr.headers['set-cookie'].join(',').match(/(Hm_1vt_6f69830ae7173059e935b61372431b35=.+?);/)[1];
+                c4 = 'Hm_lvt_6f69830ae7173059e935b61372431b35=1524617432,1524617445,1524619410,1524620683';
+                c5 = 'Hm_lpvt_6f69830ae7173059e935b61372431b35='+new Date().valueOf();
+              //console.log(c2);
+                c3 = c1+'; '+c2+ '; _gat=1';
+            //console.log(c3);
 					 	request.get('http://shanghaicity.openservice.kankanews.com/public/bus')
-	  .set(base1)
-	.set('Cookie',c3)
-	.end((err,response)=>{
+	              .set(base1)
+	             .set('Cookie',c3)
+	            .end((err,response)=>{
 							let cook = response.headers['set-cookie'].join(',');
- 	let cookie1 = cook.match(/(HA=.+?);/)[1];
+ 	            let cookie1 = cook.match(/(HA=.+?);/)[1];
 							let cookie2 =cook.match(/(HA=.+?);/)[1];
 							let cookie3 =cook.match(/(HB=.+?);/)[1];
 							let cookie4 =cook.match(/(HC=.+?);/)[1];
@@ -137,56 +136,26 @@ Host:'shanghaicity.openservice.kankanews.com',
 							let cookie8 =cook.match(/(HK=.+?);/)[1];
 							let cookie9 =cook.match(/(HO=.+?);/)[1];
 							let cookie10 =cook.match(/(HY=.+?);/)[1];
-							let cookie11 =c2;
-							let cookie12 =c4;
-							let cookie13 =c5;
 							let cookie14 =cook.match(/(Hm_p1vt_6f69830ae7173059e935b61372431b35=.+?);/)[1];
-	 let fin = [cookie1,cookie2,cookie3,cookie4,cookie5,cookie6,cookie7,cookie8,cookie9,cookie10,cookie11,cookie12,cookie13,cookie14]
+	 let fin = [cookie1,cookie2,cookie3,cookie4,cookie5,cookie6,cookie7,cookie8,cookie9,cookie10,c2,c4,c5,cookie14]
 	 let finC = fin.join('; ');
-							//console.log(finC);
-		//console.log(response);
-							//console.log(cookie);
-		request.post('http://shanghaicity.openservice.kankanews.com/public/bus/Getstop')
-	 // .set('Accept', '*/*')
-	 // .set('Host', 'shanghaicity.openservice.kankanews.com')
-	.set(baseH)
-	.set('Cookie',finC)	
-  // .set('Content-Type','application/x-www-form-urlencoded')
-   // .set('User-Agent', userAgent)
-	.type("form")
-	//.set('Cookie','Hm_p1vt_6f69830ae7173059e935b61372431b35=eSgsNFrgBI1ZtQehHGwBAg==; _ga=GA1.2.610622215.1524630670; _gat=1; Hm_lvt_6f69830ae7173059e935b61372431b35=1524617432,1524617445,1524619410,1524620683; Hm_1vt_6f69830ae7173059e935b61372431b35=eSgsNFrgBJ5ZtQehHGxBAg==; Hm_lpvt_6f69830ae7173059e935b61372431b35=1524630687; HH=62a591244fd688264fb6931a83f9102ebd718282; HK=4f9106f1ece1c7d5c148554763995cf9509b1556; HG=e5df8244d3b2c699c5109f44a4a5ea93564c1d9d; HA=d3a60b5acadc502d42b71f9aecc85eb52a1c14ad; HB=ZDNhNjBiNWFjYWRjNTAyZDQyYjcxZjlhZWNjODVlYjUyYTFjMTRhZA==; HC=9edfe983d41edad254d4e13890323ef3404d2296; HD=MjAxODA0MjU=; HY=MjAxODA0MjU=4f9106f1ece1c7d5c148554763995cf9509b1556e5df8244d3b2c699c5109f44a4a5ea93564c1d9dd6e9f8c8ef2d4dcdb59884ad640290bd7afc9729; HO=TWpBeE9EQTBNalU9MTJNVEUyTWpNeTMxVFc5NmFXeHNZUzgxTGpBZ0tFMWhZMmx1ZEc5emFEc2dWVHNnU1c1MFpXd2dUV0ZqSUU5VElGZ2dNVEJmTmw4MU95QmtaUzFrWlNrZ1FYQndiR1ZYWldKTGFYUXZOVE0wTGpFMUt5QW9TMGhVVFV3c0lHeHBhMlVnUjJWamEyOHBJRlpsY25OcGIyNHZOUzR3TGpNZ1UyRm1ZWEpwTHpVek15NHhPUzQwZDZlOWY4YzhlZjJkNGRjZGI1OTg4NGFkNjQwMjkwYmQ3YWZjOTcyOQ==')
-  .send({
-  stoptype:direction,
-  stopid:stopId,
-  sid:sid})
-    .end((err,json) => {
-      // 处理数据
-	console.log(json)
-    arr = JSON.parse(json.text);
-    res.json(arr)
-    })
+            request.post('http://shanghaicity.openservice.kankanews.com/public/bus/Getstop')
+                .set(baseH)
+                .set('Cookie',finC)
+                .type("form")
+                .send(para)
+                .end((err,json) => {
+                  // 处理数据
+              // console.log(json)
+                arr = JSON.parse(json.text);
+                res.json(arr)
+                })
   
-  })
-	})
-		
-		
-		
+            })
+	        })	
 		})
-
-	
-
-
-
-
-  //axios.post('http://shanghaicity.openservice.kankanews.com/public/bus/Getstop',para,{headers: {'Content-Type': 'application/x-www-form-urlencoded','User-Agent':'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'},})
-  //.then(json => {
-  //   arr = json.data
-  //}).then(()=>{
-   // res.json(arr)
-  //})
-  //.catch(err => console.log(err))
 });
-
+//星座运势
 app.get('/astro/fortune/:astroid', (req,res,next)=>{
   let astroid = req.params.astroid;
   let arr
