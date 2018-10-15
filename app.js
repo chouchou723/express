ada324d44864116f335d4f6479f7cfb57a234d00
ChouChou <527835300@qq.com>
2018-10-15 10:36:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.get('/busstop/:sid', (req,res,next)=>{
   }
 )
   .then(json => {
+	  console.log(json)
     	let arr = json.data
 	let $ = cheerio.load(arr);
         let start_stop = $('.upgoing p span').first().text().trim();
