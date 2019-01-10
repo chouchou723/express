153fe2e313e8cb3a747c359690ed27cee99445e4
ChouChou <527835300@qq.com>
2019-01-10 11:52:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ let url = 'https://shanghaicity.openservice.kankanews.com/public/bus';
 // 			console.log($(this).html())
 			if($(this).html().indexOf('119')>-1){
 				let t = $(this).html().replace(/[\r\n]/g,'').match(/data.*/g)[0].split('=')[1].replace(/\s/g,'');
-			list = t.split('[')[1].split(']')[0].split(',')
+			list = (t.split('[')[1].split(']')[0]).split(',')
 				return;
 			}
 		})
