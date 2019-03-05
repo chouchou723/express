bce4c125a3a9282f905e67fc18974ab2c5ee0785
ChouChou <527835300@qq.com>
2019-03-05 14:22:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -312,7 +312,7 @@ app.get('/busname/:name', (req, res, next) => {
     }
 //         .type("form")
 
-   request.post('https://shanghaicity.openservice.kankanews.com/public/bus/get').type('form').set(base1).send('idnum=119路'}).end((err, resp) => {
+   request.post('https://shanghaicity.openservice.kankanews.com/public/bus/get').type('form').set(base1).send('idnum=119路').end((err, resp) => {
        console.log(resp)
            if (err) {
                res.sendStatus(500);
