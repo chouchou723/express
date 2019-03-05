6f59889551e4067c1fd5f8d803f8902d196273df
ChouChou <527835300@qq.com>
2019-03-05 15:16:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -294,7 +294,8 @@ app.get('/busstop/:sid', (req, res, next) => {
 app.get('/busname/:name', (req, res, next) => {
  let name = req.params.name//.split('%').length===1?req.params.name.split('%')[0]:req.params.name.split('%')[0]+'路';
 //    let name = rlencode.parse(‘idnum:’+req.params.name, {charset: 'utf-8'});
-    console.log(typeof name);
+    console.log(rlencode(name, 'gbk'));
+    console.log(rlencode(name, 'utf-8'));
     let base1 = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
