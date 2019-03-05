572ca5c959fefcf04a07e253b2d19fb50447b8ad
ChouChou <527835300@qq.com>
2019-03-05 15:01:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -296,15 +296,15 @@ app.get('/busname/:name', (req, res, next) => {
 //    let name = rlencode.parse(‘idnum:’+req.params.name, {charset: 'utf-8'});
     console.log(name);
     let base1 = {
-        Accept: '*/*',
+        'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,,zh;q=0.9,en;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         //   'Cache-Control':'no-cache',
-        Connection: 'keep-alive',
+        'Connection': 'keep-alive',
         'Content-Length': 42,
-        Host: 'shanghaicity.openservice.kankanews.com',
+        'Host': 'shanghaicity.openservice.kankanews.com',
 //        Origin: 'https://shanghaicity.openservice.kankanews.com',
-        Referer: 'https://shanghaicity.openservice.kankanews.com/public/bus',
+        'Referer': 'https://shanghaicity.openservice.kankanews.com/public/bus',
         // Pragma:'no-cache',
         //   Host:'shanghaicity.openservice.kankanews.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; de-de) AppleWebKit/534.15+ (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4',
@@ -312,7 +312,7 @@ app.get('/busname/:name', (req, res, next) => {
     }
 //         .type("form")
 
-   request.post('https://shanghaicity.openservice.kankanews.com/public/bus/get').set(base1).type('form').send({idnum:name+''}).then((resp) => {
+   request.post('https://shanghaicity.openservice.kankanews.com/public/bus/get').set(base1).send(`idnum:${name}`).then((resp) => {
        console.log(resp)
 //            if (err) {
 //                res.sendStatus(500);
